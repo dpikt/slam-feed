46837b11b5e41d7b63e02b17b4c1a3bd1f0ffbd7
David Pickart <davidstoreypickart@gmail.com>
2020-02-19 18:41:49 -0600

    Parse heroku env

--- config/db.js
+++ config/db.js
@@ -1,15 +1,31 @@
 const { Pool } = require('pg')
+const url = require('url')
 
-const pool = new Pool({
-  user: 'dpikt',
-  // password: PGPASSWORD,
-  database: 'slams',
-  // port: PGPORT,
-  // host: PGHOST,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  // ssl: PGSSL,
-})
+function parseHerokuEnv(databaseUrl) {
+  const params = url.parse(databaseUrl)
+  const auth = params.auth.split(':')
+  return {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true,
+  }
+}
+
+const pool = process.env.DATABASE_URL
+  ? new Pool(parseHerokuEnv(process.env.DATABASE_URL))
+  : new Pool({
+      user: 'dpikt',
+      // password: PGPASSWORD,
+      database: 'slams',
+      // port: PGPORT,
+      // host: PGHOST,
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+      // ssl: PGSSL,
+    })
 
 // Note used currently...
 // function end() {
